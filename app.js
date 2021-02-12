9fd806a8fad62c3db4d43a456bc32b18ba06ed56
ksh0003 <70246440+ksh0003@users.noreply.github.com>
2021-02-12 16:38:03 +1100

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const fr = 60;
+
+function preload() {
+
+    frameRate(fr);
+
+}
+
+function setup() {
+
+    createCanvas(1200,700);
+
+}
+
+function draw() {
+
+    background(255)
+    drawSprites();
+
+
+
+}
